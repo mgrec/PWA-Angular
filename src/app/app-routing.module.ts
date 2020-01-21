73834b01db43a10256839408ddfe7cd6d676d98c
subskill-mgrec <m.grec@subskill.com>
2020-01-21 10:25:13 +0100

    route, index & co

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleComponent } from './people/people.component';
+import { PlanetComponent } from './planet/planet.component';
+import { StarshipsComponent } from './starships/starships.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'people', component: PeopleComponent },
+  { path: 'planets', component: PlanetComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: '', component: PeopleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material';
+import { PeopleComponent } from './people/people.component';
+import { PlanetComponent } from './planet/planet.component';
+import { StarshipsComponent } from './starships/starships.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    PlanetComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
